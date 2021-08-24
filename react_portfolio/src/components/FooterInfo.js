97942bf40ceac313710e18e7d73aa20a7da1cc24
kferg357 <kennethferguson90@gmail.com>
2021-08-23 22:20:24 -0400

    added footerinfo.js

---
+++ react_portfolio/src/components/FooterInfo.js
@@ -0,0 +1,20 @@
+import "./styles.css";
+	import React from "react";
+	
+
+	function FooterInfo() {
+	    return (
+	    <div>
+	        <p>&nbsp;</p>
+	        <p>&nbsp;</p>
+	        <p>&nbsp;</p>
+	        <p>&nbsp;</p>
+	    <div className="bg-dark text-white fixed-bottom footer mt-auto py-3">
+	    <span className="text-muted">Built for UNC Coding Bootcamp &bull; Right reserved under the MIT License</span>
+	    </div>
+	    </div>
+	    );
+	}
+	
+
+	export default FooterInfo;
