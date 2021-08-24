87b329a480d3f6b65e41dde915bd40859c2eef55
kferg357 <kennethferguson90@gmail.com>
2021-08-23 22:33:53 -0400

    created headerinfo

---
+++ react_portfolio/src/components/HeaderInfo.js
@@ -0,0 +1,20 @@
+import "./styles.css"
+import React from "react";
+import facepic.jpg from "./images/facepic.jpg"
+
+function HeaderInfo() {
+    return (
+        <div className="jumbotron jumbotron-fluid">
+            <div className="moreleft"><img className="profile" src={facepic} alt="facepic" />
+                <div className="headerText">
+                    <h1>Hello</h1>
+                    My name is Kenneth A. Ferguson <br />
+                    I am an innovative and deadline-driven Software Developer with experience in designing and developing user-friendly software applications with vast knowledge of debugging problems. Possesing the craft of launching final products that are polished and deliverable.  <br />
+                </div>
+            </div>
+
+        </div>
+    );
+}
+
+export default HeaderInfo;
